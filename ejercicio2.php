87b1f0bc9ba9cea43b3f69453ddfaa73c6274f05
samuelroam <samuelroam@gmail.com>
2019-09-13 11:06:38 +0200

    segundo ejercicio

---
+++ ejercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+$number1 = 2;
+$number2=6;
+$number3=1;
+if($number1>$number2&&$number1>$number3){
+echo("El mayor numero es el primero");
+}else if($number2>$number1&&$number2>$number3){
+echo("El mayor numero es el segundo");
+}else{
+echo("El mayor numero es el tercero");
+}
+?>
+</body>
+</html>
