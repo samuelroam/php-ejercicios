8bb3af6921869680183999b9f128074723bd5d9e
samuelroam <samuelroam@gmail.com>
2019-09-30 16:34:15 +0200

    ejercicio 1 actualizar

--- bloque2/ejercicio1.php
+++ bloque2/ejercicio1.php
@@ -8,7 +8,6 @@
     
     $naturales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	$factoriales = [];
-	$naturalFactorizado = 0;
 
     for ($i=0; $i < count($naturales); $i++) { 
 		array_push($factoriales, Factorial($naturales[$i]));
