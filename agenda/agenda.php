4e9fb87b6849f66eb1d94ff00d210e02e1228696
samuelroam <samuelroam@gmail.com>
2019-10-04 10:21:52 +0200

    agenda

--- agenda/agenda.php
+++ agenda/agenda.php
@@ -5,22 +5,39 @@
 </head>
 <body>
     <?php
-    $nombre = strtolower($_POST["nombre"]);   
-    $correo = strtolower($_POST["correo"]); 
+    $name=[$_POST["nombre"]];
+    $_SESSION["nombre"] = $name;
+    //strtolower($_POST["nombre"]);   
+    $_SESSION["correo"] = strtolower($_POST["correo"]); 
+    echo "Usted está registrado como ".$_SESSION["nombre"];
+    $array=array($_POST["nombre"],$_POST["correo"]);
+    //$_SESSION["completo"]=;
+    array_push($_SESSION["completo"],$array);
+    $complete=$_SESSION["completo"];
     ?>
+    
     <form action="#" method="post">
         <p>Introduzca su nombre <input type="text" name="nombre"></p>
         <br>
         <p>Introduzca su correo <input type="text" name="correo" placeholder="emilio"></p>
         <br>
-        <input type="submit">
+        <input type="submit" name="submit">
         <br><br>
         <?php
-        if($nombre==""){
-            echo "<b>Por favor, introduzca un nombre</b>";
-        }
-        echo $nombre;
-        echo $correo;
+        if (isset($_POST["submit"])){
+            if($_SESSION["nombre"]==""){
+                echo "<b>Por favor, introduzca un nombre</b>";
+            };
+           // for($i=0;$i<=1;$i++){
+             //   echo $name[i];
+            echo $_SESSION["nombre"];
+            echo "<br>";
+            echo $_SESSION["correo"];
+        //}
+       // foreach($complete as $asdf){
+            print_r($_SESSION["completo"]);
+        //}
+    }
         ?>
 </form>
 </body>
